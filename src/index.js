d197e5f6004ef956dfcc9ef0b752637061c87e9b
bilgicgurkan <bilgicgurkan44@gmail.com>
2024-01-21 19:29:33 +0200

    created fetch method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { Request } from "./request";
+import { Front } from "./front";
 
 const request = new Request();
+const front = new Front();
 
 eventListener();
 
@@ -9,11 +11,19 @@ function eventListener() {
     document.querySelector("#save-list").addEventListener("click", addNewItem);
 }
 
-function loadList(){
+function loadList() {
     request.get()
-    .then(allList => {
-        console.log(allList)
-    }) 
+        .then(allList => {
+            // console.log(allList)
+            // front.loadAllList(allList); burası da olabilir aşağıdaki şart da olabilir.
+            if (allList.lenght === null) {
+                console.log("selam")
+            }
+            else {
+                front.loadAllList(allList)
+            }
+        })
+        .catch(err => console.log(err))
 }
 
 function addNewItem() {
@@ -21,11 +31,11 @@ function addNewItem() {
         console.log("burası boş");
     }
     else {
-        request.post({name : document.querySelector("#name-input").value.trim(), unit : document.querySelector("#unit-input").value.trim(), price : document.querySelector("#price-input").value.trim()})
-        .then(data => {
-            console.log(data)
-        })
-        .catch(err => console.log(err))
+        request.post({ name: document.querySelector("#name-input").value.trim(), unit: document.querySelector("#unit-input").value.trim(), price: document.querySelector("#price-input").value.trim() })
+            .then(addList => {
+                console.log(addList)
+            })
+            .catch(err => console.log(err))
 
         // console.log(document.querySelector("#name-input").value.trim());
         // console.log(document.querySelector("#unit-input").value.trim());
